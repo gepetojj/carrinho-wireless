d8bd67c74219a6655c17dd0cbc6ea3e4e610fcb5
João Pedro <nmjoaopedro22@gmail.com>
2023-09-16 17:13:25 -0300

    feat: adição de suporte para WiFi e ESP-NOW e painel de controle

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <AsyncTCP.h>
 #include <DNSServer.h>
 #include <esp_now.h>
-#include <DistanceSensor.hh>
+// #include <DistanceSensor.hh>
 #include <Motor.hh>
 #include <Preferences.h>
 #include <WiFi.h>
@@ -11,9 +11,7 @@
 #include "pages.hh"
 
 #define DEVICE_NAME "Carrinho Wireless"
-#define PREF_SETUP_KEY "setupDone"
-#define PREF_SSID_KEY "lastSSID"
-#define PREF_PASS_KEY "lastPASS"
+#define PREF_WIFI_SETUP_KEY "wifiMenu"
 
 #define LEFT_WHEEL_IN1_PIN 32
 #define LEFT_WHEEL_IN2_PIN 33
@@ -32,18 +30,15 @@
 Motor leftWheel(LEFT_WHEEL_IN1_PIN, LEFT_WHEEL_IN2_PIN, LEFT_WHEEL_VEL_PIN, false);
 Motor rightWheel(RIGHT_WHEEL_IN1_PIN, RIGHT_WHEEL_IN2_PIN, RIGHT_WHEEL_VEL_PIN, false);
 
-DistanceSensor frontSensor(FRONT_SENSOR_TRIG_PIN, FRONT_SENSOR_ECHO_PIN);
+// DistanceSensor frontSensor(FRONT_SENSOR_TRIG_PIN, FRONT_SENSOR_ECHO_PIN);
 
 DynamicJsonDocument doc(1024);
-DNSServer dns;
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 Preferences preferences;
 
-bool SETUP_DONE;
-bool TRY_CONNECTION;
-String SSID;
-String PASS;
+bool WIFI_MENU_SELECTED;
+bool ERASING_MEMORY = false;
 
 u_int16_t MOTORS_VELOCITY = 255;
 u_int8_t FORWARD_MOVEMENT_LIMIT = 17;
@@ -51,112 +46,53 @@ u_int8_t FORWARD_MOVEMENT_LIMIT = 17;
 typedef struct ControllerPayload
 {
 	uint8_t direction;
+	uint8_t command;
 } ControllerPayload;
 
 ControllerPayload CONTROLLER_PAYLOAD;
 
 // !! Métodos auxiliares para conexão ao WiFi !!
 
-class CaptiveRequestHandler : public AsyncWebHandler
-{
-public:
-	CaptiveRequestHandler() {}
-	virtual ~CaptiveRequestHandler() {}
-
-	bool canHandle(AsyncWebServerRequest *req)
-	{
-		return true;
-	}
-
-	void handleRequest(AsyncWebServerRequest *req)
-	{
-		req->send_P(200, "text/html", CAPTIVE_PAGE);
-	}
-};
-
-void setupCaptiveServer()
-{
-	server.on("/", HTTP_GET, [](AsyncWebServerRequest *req)
-			  { req->send_P(200, "text/html", CAPTIVE_PAGE); });
-
-	server.on("/set", HTTP_GET, [](AsyncWebServerRequest *req)
-			  {
-		if (req->hasParam("ssid") && req->hasParam("pass")) {
-			SSID = req->getParam("ssid")->value();
-			PASS = req->getParam("pass")->value();
-			TRY_CONNECTION = true;
-			Serial.println("[CaptiveServer] Dados recebidos.");
-			req->send(200, "text/plain", "Seus dados foram recebidos, agora o carrinho está tentando se conectar.");
-		} else {
-			req->send(400, "text/plain", "SSID e/ou Senha não foram recebidos, tente novamente.");
-		} });
-}
-
-void startCaptivePortal()
+void startWiFiMenu()
 {
 	WiFi.mode(WIFI_AP);
 	WiFi.softAP(DEVICE_NAME);
-	Serial.println("[CaptivePortal] Modo WiFi alterado para AP.");
-
-	setupCaptiveServer();
-	dns.start(53, "*", WiFi.softAPIP());
-	server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);
-	server.begin();
-
-	Serial.println("[CaptivePortal] Servidor e DNS iniciados.");
+	Serial.println("[WiFi Menu] Modo WiFi alterado para AP.");
 }
 
-void tryWiFiConnection()
+void registerWiFiMenu()
 {
-	// WiFi.mode(WIFI_AP_STA);
-	WiFi.mode(WIFI_STA);
-	WiFi.setSleep(false);
-	Serial.println("[WiFi] Modo WiFi alterado para STA.");
-	Serial.print("[WiFi] Tentando conectar-se ao SSID: ");
-	Serial.println(SSID);
+	server.reset();
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *req)
+			  { req->send_P(200, "text/html", MAIN_PAGE); });
+	server.on("/console", HTTP_GET, [](AsyncWebServerRequest *req)
+			  { req->send_P(200, "text/html", CONSOLE_PAGE); });
+	server.on("/changeVel", HTTP_GET, [](AsyncWebServerRequest *req)
+			  {
+		if (req->hasParam("vel")) {
+			const int velocity = std::atoi(req->getParam("vel")->value().c_str());
+			MOTORS_VELOCITY = 255 * velocity / 100;
+			Serial.printf("[Motors] Velocidade alterada para %i\n", MOTORS_VELOCITY);
+			req->send(200, "text/plain", "Seus dados foram recebidos.");
+		} else {
+			req->send(400, "text/plain", "Velocidade não foi recebida, tente novamente.");
+		} });
 
-	const char *ssid = SSID.c_str();
-	const char *pass = PASS.c_str();
-	WiFi.begin(ssid, pass);
+	server.begin();
 
-	unsigned short counter = 0;
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		counter++;
-		delay(1000);
-
-		if (counter >= 10)
-		{
-			Serial.println("[WiFi] Maximo de tentativas atingido, dados podem estar incorretos.");
-			SETUP_DONE = false;
-			TRY_CONNECTION = false;
-			preferences.putBool(PREF_SETUP_KEY, SETUP_DONE);
-
-			startCaptivePortal();
-			break;
-		}
-	}
-
-	if (WiFi.status() == WL_CONNECTED)
-	{
-		SETUP_DONE = true;
-		preferences.putBool(PREF_SETUP_KEY, true);
-
-		String ssid_check = preferences.getString(PREF_SSID_KEY, "");
-		String pass_check = preferences.getString(PREF_PASS_KEY, "");
-
-		if (ssid_check != SSID)
-		{
-			preferences.putString(PREF_SSID_KEY, SSID);
-		}
-		if (pass_check != PASS)
-		{
-			preferences.putString(PREF_PASS_KEY, PASS);
-		}
-	}
+	Serial.print("[WiFi Menu] Conectado. IP: ");
+	Serial.println(WiFi.localIP());
+	Serial.print("[WiFi Menu] MAC: ");
+	Serial.println(WiFi.macAddress());
 }
 
 // !! Métodos do WebSocket !!
+
+void startENMenu()
+{
+	WiFi.mode(WIFI_STA);
+	Serial.println("[EN Menu] Modo WiFi alterado para STA.");
+}
 
 void onWSMessageHandler(void *arg, uint8_t *data, size_t len)
 {
@@ -191,6 +127,17 @@ void onWSMessageHandler(void *arg, uint8_t *data, size_t len)
 			leftWheel.stopMovement();
 			rightWheel.stopMovement();
 		}
+
+		else if (strcmp((char *)data, "enmenu") == 0)
+		{
+			WIFI_MENU_SELECTED = false;
+			preferences.putBool(PREF_WIFI_SETUP_KEY, false);
+			startENMenu();
+		}
+		else if (strcmp((char *)data, "reset") == 0)
+		{
+			ERASING_MEMORY = true;
+		}
 	}
 }
 
@@ -207,6 +154,7 @@ void onEventHandler(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEve
 		break;
 
 	default:
+		Serial.println("[WebSocket] Um evento nao tratado foi disparado.");
 		break;
 	}
 }
@@ -244,6 +192,13 @@ void onENMessageHandler(const uint8_t *mac, const uint8_t *data, int len)
 		leftWheel.stopMovement();
 		rightWheel.stopMovement();
 	}
+
+	if (CONTROLLER_PAYLOAD.command == 0x09)
+	{
+		WIFI_MENU_SELECTED = true;
+		preferences.putBool(PREF_WIFI_SETUP_KEY, true);
+		startWiFiMenu();
+	}
 }
 
 // !! Métodos do lifecycle do microcontrolador !!
@@ -253,9 +208,10 @@ void reset(void *)
 	Serial.printf("[Reset] Iniciando tarefa no core %i\n", xPortGetCoreID());
 	while (true)
 	{
-		if (digitalRead(RESET_PIN) == HIGH)
+		if (digitalRead(RESET_PIN) == HIGH || ERASING_MEMORY == true)
 		{
-			Serial.println("[Reset] Reset pressionado!! Apagando memoria em 12 segundos.");
+			ERASING_MEMORY = true;
+			Serial.println("[Reset] Reset acionado!! Apagando memoria em 12 segundos.");
 
 			for (int x = 0; x < 10; x++)
 			{
@@ -271,41 +227,6 @@ void reset(void *)
 	}
 }
 
-void sensorsAlarmTask(void *)
-{
-	Serial.printf("[SensorsAlarm] Iniciando tarefa no core %i\n", xPortGetCoreID());
-	while (true)
-	{
-		// Cancela movimento para frente se limite for atingido
-		if (doc["frontSensor"] <= FORWARD_MOVEMENT_LIMIT)
-		{
-			Serial.println("[Motors] Movimento para frente cancelado (distancia).");
-			leftWheel.stopMovement();
-			rightWheel.stopMovement();
-		}
-
-		// Alerta com o buzzer dependendo da distância do sensor
-		if (doc["frontSensor"] <= 12)
-		{
-			analogWrite(BUZZER_PIN, 120);
-			vTaskDelay(100 / portTICK_PERIOD_MS);
-			analogWrite(BUZZER_PIN, LOW);
-		}
-		else if (doc["frontSensor"] > 12 && doc["frontSensor"] <= 20)
-		{
-			analogWrite(BUZZER_PIN, 120);
-			vTaskDelay(200 / portTICK_PERIOD_MS);
-			analogWrite(BUZZER_PIN, LOW);
-		}
-		else if (doc["frontSensor"] > 20 && doc["frontSensor"] <= 28)
-		{
-			analogWrite(BUZZER_PIN, 120);
-			vTaskDelay(400 / portTICK_PERIOD_MS);
-			analogWrite(BUZZER_PIN, LOW);
-		}
-	}
-}
-
 void setup()
 {
 	Serial.begin(115200);
@@ -316,70 +237,29 @@ void setup()
 
 	// Inicia tarefas
 	xTaskCreatePinnedToCore(reset, "Reset", 4000, NULL, 0, NULL, 0);
-	xTaskCreatePinnedToCore(sensorsAlarmTask, "SensorsAlarm", 8000, NULL, 0, NULL, 0);
+	// xTaskCreatePinnedToCore(sensorsAlarmTask, "SensorsAlarm", 8000, NULL, 0, NULL, 0);
 
-	// SETUP_DONE corresponde a existência de SSID && PASS
-	SETUP_DONE = preferences.getBool(PREF_SETUP_KEY, false);
-	SSID = preferences.getString(PREF_SSID_KEY, "");
-	PASS = preferences.getString(PREF_PASS_KEY, "");
-
-	if (SETUP_DONE)
+	// Se menu WiFi estiver selecionado, inicia WiFi e as páginas do servidor
+	// Se não, registra as páginas do servidor e inicia somente o ESP-NOW
+	WIFI_MENU_SELECTED = preferences.getBool(PREF_WIFI_SETUP_KEY, true);
+	if (WIFI_MENU_SELECTED)
 	{
-		Serial.println("[Setup] Usando dados inseridos previamente...");
-		Serial.print("[Setup] SSID: ");
-		Serial.println(SSID);
-		Serial.print("[Setup] PASS: ");
-		Serial.println(PASS);
-		tryWiFiConnection();
+		startWiFiMenu();
 	}
 	else
 	{
-		Serial.println("[Setup] Nenhum dado previo foi encontrado, iniciando Captive Portal.");
-		startCaptivePortal();
+		startENMenu();
 	}
 
-	Serial.println("[Setup] Aguardando Captive Portal.");
-	while (!SETUP_DONE)
-	{
-		dns.processNextRequest();
-		delay(10);
-
-		if (TRY_CONNECTION)
-		{
-			Serial.println("[Setup] Tentando conectar-se com dados recebidos do usuario.");
-			tryWiFiConnection();
-		}
-	}
-
-	// Remove os handlers do Captive Server
-	server.reset();
-	server.on("/", HTTP_GET, [](AsyncWebServerRequest *req)
-			  { req->send_P(200, "text/html", MAIN_PAGE); });
-	server.on("/changeVel", HTTP_GET, [](AsyncWebServerRequest *req)
-			  {
-		if (req->hasParam("vel")) {
-			const int velocity = std::atoi(req->getParam("vel")->value().c_str());
-			MOTORS_VELOCITY = 255 * velocity / 100;
-			Serial.printf("[Motors] Velocidade alterada para %i\n", MOTORS_VELOCITY);
-			req->send(200, "text/plain", "Seus dados foram recebidos.");
-		} else {
-			req->send(400, "text/plain", "Velocidade não foi recebido, tente novamente.");
-		} });
-	server.begin();
-
+	registerWiFiMenu();
 	// Inicializa o servidor WebSocket
 	ws.onEvent(onEventHandler);
 	server.addHandler(&ws);
 
-	Serial.print("[Setup] Conectado. IP: ");
-	Serial.println(WiFi.localIP());
-	Serial.print("[Setup] MAC: ");
-	Serial.println(WiFi.macAddress());
-
 	// Inicializa o ESP-NOW
 	if (esp_now_init() != ESP_OK)
 	{
-		Serial.println("[Setup] Erro iniciando o procotolo ESP-NOW.");
+		Serial.println("[EN Menu] Erro iniciando o procotolo ESP-NOW.");
 		return;
 	}
 
@@ -390,8 +270,15 @@ void loop()
 {
 	ws.cleanupClients();
 
-	// Envia para os clientes as leituras dos sensores em cm
-	doc["frontSensor"] = frontSensor.readDistance();
+	doc["wifiMenuSelected"] = WIFI_MENU_SELECTED;
+	doc["erasingMemory"] = ERASING_MEMORY;
+	doc["motorsVelocity"] = MOTORS_VELOCITY;
+	doc["wifiStatus"] = WiFi.status();
+	doc["espHeap"] = ESP.getHeapSize();
+	doc["espFreeHeap"] = ESP.getFreeHeap();
+	doc["clients"] = ws.count();
+	doc["ip"] = WiFi.localIP();
+	doc["mac"] = WiFi.macAddress();
 
 	String obj = "";
 	serializeJson(doc, obj);
